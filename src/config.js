e8ea9a348f2a7c56fb4d7cdbd214d3eb302e21fd
Alex <wertekq@gmail.com>
2024-11-16 17:07:50 +0000

    update 2 16.11

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import { getConfig } from './config';
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
+
+export default firebaseConfig;
