689ecdbf40df3aaa5a32bc264ba1ac968b194509
Alex <wertekq@gmail.com>
2025-02-15 16:40:11 +0000

    update 4 15.02

--- src/config.js
+++ src/config.js
@@ -3,10 +3,12 @@ import { getConfig } from './config';
 const firebaseConfig = {
   apiKey: "your-api-key",
   authDomain: "your-app.firebaseapp.com",
+  databaseURL: "your-url-app.firebaseio.com",
   projectId: "your-project-id",
   storageBucket: "your-app.appspot.com",
   messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
 };
 
 export default firebaseConfig;
