3d1468e4813d1e58876786cf10c713e312ee03c7
Alex <wertekq@gmail.com>
2026-03-20 22:09:37 +0000

    Rebuild public site for AI agency positioning

---
+++ solshr.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector('.nav-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+if (navToggle && siteNav) {
+    navToggle.addEventListener('click', () => {
+        const isOpen = siteNav.classList.toggle('open');
+        navToggle.setAttribute('aria-expanded', String(isOpen));
+    });
+}
